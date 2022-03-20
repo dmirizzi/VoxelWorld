e6f60e9c7650b6404ec7cc36703e61c85e2448c8
Dario Mirizzi <dario.mirizzi@googlemail.com>
2022-03-20 02:58:05 +0100

    Added support for transparent voxels

--- Assets/Scripts/WorldGen.cs
+++ Assets/Scripts/WorldGen.cs
@@ -42,6 +42,15 @@ public static class VoxelInfo
         }
     }
 
+    public static float GetVoxelHeightOffset(VoxelType voxelType)
+    {
+        switch(voxelType)
+        {
+            case VoxelType.Water:       return 0.075f;
+            default:                    return 0.0f;
+        }
+    }
+
     public static Vector2 GetAtlasUVOffsetForVoxel(VoxelType voxelType, VoxelFace face)
     {
         var tilePosX = 0;
@@ -89,6 +98,8 @@ public class WorldGen : MonoBehaviour
 {
     public Material TextureAtlasMaterial;
 
+    public Material TextureAtlasTransparentMaterial;
+
     public const int ChunkSize = 32;
     public const float VoxelSize = 1f;
 
@@ -103,7 +114,6 @@ public class WorldGen : MonoBehaviour
     {
         var chunk = GetChunkFromVoxelPosition(x, y, z, true);
         var chunkLocalPos = GlobalToChunkLocalVoxelPos(new Vector3Int(x, y, z));
-        Debug.Log($"Setting voxel @ {x},{y},{z} -> chunk {chunk} -> chunkLocal {chunkLocalPos}");
         chunk[chunkLocalPos.x, chunkLocalPos.y, chunkLocalPos.z] = (byte)type;
     }
 
@@ -185,91 +195,129 @@ public class WorldGen : MonoBehaviour
         );
     }
 
+    private bool VoxelSideVisible(VoxelType voxelType, Vector3Int voxelPos, Vector3Int direction)
+    {
+        var neighbor = GetVoxel(voxelPos + direction);
+
+        if(VoxelInfo.IsSolid(voxelType))
+        {
+            // Solid neighboring voxels hide their shared face
+            return !VoxelInfo.IsSolid(neighbor);
+        }
+        else
+        {
+            // Transparent neighboring voxels only hide their shared face if they are of the same type
+            return voxelType != neighbor;
+        }
+    }
+
     public void Build()
     {
         foreach(var chunkPos in _chunks.Keys)
         {
             var chunkData = _chunks[chunkPos];
-            var chunkGameObj = new GameObject($"Chunk[{chunkPos.x}|{chunkPos.y}|{chunkPos.z}]");
 
+            var chunkGameObj = new GameObject($"Chunk[{chunkPos.x}|{chunkPos.y}|{chunkPos.z}]");
             chunkGameObj.AddComponent<MeshRenderer>();
             var mesh = chunkGameObj.AddComponent<MeshFilter>().mesh;
 
             var chunkVertices = new List<Vector3>();
-            var normals = new List<Vector3>();
-            var uvs = new List<Vector2>();
-            var triangles = new List<int>();
+            var chunkNormals = new List<Vector3>();
+            var chunkUVs = new List<Vector2>();
+            var chunkTriangles = new List<int>();
+
+            var chunkTpGameObj = new GameObject($"ChunkTP[{chunkPos.x}|{chunkPos.y}|{chunkPos.z}]");
+            chunkTpGameObj.AddComponent<MeshRenderer>();
+            var meshTp = chunkTpGameObj.AddComponent<MeshFilter>().mesh;
+
+            var chunkVerticesTp = new List<Vector3>();
+            var chunkNormalsTp = new List<Vector3>();
+            var chunkUVsTp = new List<Vector2>();
+            var chunkTrianglesTp = new List<int>();
 
             var chunkVoxelPos = ChunkToBaseVoxelPos(chunkPos);
+
             for(int x = 0; x < ChunkSize; ++x)
             {
                 for(int y = 0; y < ChunkSize; ++y)
                 {
                     for(int z = 0; z < ChunkSize; ++z)
                     {
-                        if(chunkData[x, y, z] == (byte)VoxelType.Empty) continue;
-                        
+                        var voxelType = (VoxelType)chunkData[x, y, z];
+
+                        if(voxelType == VoxelType.Empty) continue;
+                        bool isTransparent = !VoxelInfo.IsSolid((VoxelType)chunkData[x, y, z]);
+
+                        var vertices = isTransparent ? chunkVerticesTp : chunkVertices;
+                        var normals = isTransparent ? chunkNormalsTp : chunkNormals;
+                        var uvs = isTransparent ? chunkUVsTp : chunkUVs;
+                        var triangles = isTransparent ? chunkTrianglesTp : chunkTriangles;
+
+                        var heightOffset = VoxelInfo.GetVoxelHeightOffset(voxelType);
+
                         var v = new Vector3[]
                         {
                              new Vector3(x, y, z),
                              new Vector3(x + VoxelSize, y, z),
                              new Vector3(x + VoxelSize, y, z + VoxelSize),
                              new Vector3(x, y, z + VoxelSize),
-                             new Vector3(x, y + VoxelSize, z),
-                             new Vector3(x + VoxelSize, y + VoxelSize, z),
-                             new Vector3(x + VoxelSize, y + VoxelSize, z + VoxelSize),
-                             new Vector3(x, y + VoxelSize, z + VoxelSize)
+
+                             new Vector3(x, y + VoxelSize - heightOffset, z),
+                             new Vector3(x + VoxelSize, y + VoxelSize - heightOffset, z),
+                             new Vector3(x + VoxelSize, y + VoxelSize - heightOffset, z + VoxelSize),
+                             new Vector3(x, y + VoxelSize - heightOffset, z + VoxelSize)
                         };
 
-                        var vertices = new List<Vector3>();
+                        var voxelVertices = new List<Vector3>();
                         var voxelPos = chunkVoxelPos + new Vector3Int(x, y, z);
-                        if(!VoxelInfo.IsSolid(GetVoxel(voxelPos + Vector3Int.down)))
+
+                        if(VoxelSideVisible(voxelType, voxelPos, Vector3Int.down))
                         {
                             var uv = GetUVsForTile((VoxelType)chunkData[x, y, z], VoxelFace.Bottom);
-                            vertices.AddRange(new Vector3[]{ v[0], v[1], v[2], v[3] });
+                            voxelVertices.AddRange(new Vector3[]{ v[0], v[1], v[2], v[3] });
                             normals.AddRange(Enumerable.Repeat(Vector3.down, 4));
                             uvs.AddRange(new Vector2[]{ uv[0], uv[1], uv[3], uv[2] });
                         }        
-                        if(!VoxelInfo.IsSolid(GetVoxel(voxelPos + Vector3Int.up)))          
+                        if(VoxelSideVisible(voxelType, voxelPos, Vector3Int.up))          
                         {
                             var uv = GetUVsForTile((VoxelType)chunkData[x, y, z], VoxelFace.Top);
-                            vertices.AddRange(new Vector3[]{ v[5], v[4], v[7], v[6] });                        
+                            voxelVertices.AddRange(new Vector3[]{ v[5], v[4], v[7], v[6] });                        
                             normals.AddRange(Enumerable.Repeat(Vector3.up, 4));
                             uvs.AddRange(new Vector2[]{ uv[3], uv[2], uv[0], uv[1] });
                         }
-                        if(!VoxelInfo.IsSolid(GetVoxel(voxelPos + Vector3Int.back)))        
+                        if(VoxelSideVisible(voxelType, voxelPos, Vector3Int.back))        
                         {
-                            var uv = GetUVsForTile((VoxelType)chunkData[x, y, z], VoxelFace.Back);
-                            vertices.AddRange(new Vector3[]{ v[0], v[4], v[5], v[1] });
+                            var uv = GetUVsForTile((VoxelType)chunkData[x, y, z], VoxelFace.Front);
+                            voxelVertices.AddRange(new Vector3[]{ v[0], v[4], v[5], v[1] });
                             normals.AddRange(Enumerable.Repeat(Vector3.back, 4));
                             uvs.AddRange(new Vector2[]{ uv[2], uv[0], uv[1], uv[3] });
                         }
-                        if(!VoxelInfo.IsSolid(GetVoxel(voxelPos + Vector3Int.forward)))     
+                        if(VoxelSideVisible(voxelType, voxelPos, Vector3Int.forward))     
                         {
-                            var uv = GetUVsForTile((VoxelType)chunkData[x, y, z], VoxelFace.Front);
-                            vertices.AddRange(new Vector3[]{ v[3], v[2], v[6], v[7] });
+                            var uv = GetUVsForTile((VoxelType)chunkData[x, y, z], VoxelFace.Back);
+                            voxelVertices.AddRange(new Vector3[]{ v[3], v[2], v[6], v[7] });
                             normals.AddRange(Enumerable.Repeat(Vector3.forward, 4));
                             uvs.AddRange(new Vector2[]{ uv[3], uv[2], uv[0], uv[1] });
                         }
-                        if(!VoxelInfo.IsSolid(GetVoxel(voxelPos + Vector3Int.left)))        
+                        if(VoxelSideVisible(voxelType, voxelPos, Vector3Int.left))        
                         {
                             var uv = GetUVsForTile((VoxelType)chunkData[x, y, z], VoxelFace.Left);
-                            vertices.AddRange(new Vector3[]{ v[4], v[0], v[3], v[7] });
+                            voxelVertices.AddRange(new Vector3[]{ v[4], v[0], v[3], v[7] });
                             normals.AddRange(Enumerable.Repeat(Vector3.left, 4));
                             uvs.AddRange(new Vector2[]{ uv[1], uv[3], uv[2], uv[0] });
                         }
-                        if(!VoxelInfo.IsSolid(GetVoxel(voxelPos + Vector3Int.right)))       
+                        if(VoxelSideVisible(voxelType, voxelPos, Vector3Int.right))       
                         {
                             var uv = GetUVsForTile((VoxelType)chunkData[x, y, z], VoxelFace.Right);
-                            vertices.AddRange(new Vector3[]{ v[5], v[6], v[2], v[1] });
+                            voxelVertices.AddRange(new Vector3[]{ v[5], v[6], v[2], v[1] });
                             normals.AddRange(Enumerable.Repeat(Vector3.right, 4));
                             uvs.AddRange(new Vector2[]{ uv[0], uv[1], uv[3], uv[2] });
                         }
 
-                        int vertexBaseIdx = chunkVertices.Count;
-                        chunkVertices.AddRange(vertices);
+                        int vertexBaseIdx = vertices.Count;
+                        vertices.AddRange(voxelVertices);
 
-                        for(int i = 0; i < vertices.Count; i += 4)
+                        for(int i = 0; i < voxelVertices.Count; i += 4)
                         {
                             triangles.Add(i + vertexBaseIdx);
                             triangles.Add(i + 1 + vertexBaseIdx);
@@ -284,13 +332,23 @@ public class WorldGen : MonoBehaviour
 
             mesh.Clear();
             mesh.vertices = chunkVertices.ToArray();
-            mesh.triangles = triangles.ToArray();
-            mesh.normals = normals.ToArray();
-            mesh.uv = uvs.ToArray();
+            mesh.triangles = chunkTriangles.ToArray();
+            mesh.normals = chunkNormals.ToArray();
+            mesh.uv = chunkUVs.ToArray();
             mesh.Optimize();
 
             chunkGameObj.transform.position = chunkVoxelPos;
             chunkGameObj.GetComponent<Renderer>().material = TextureAtlasMaterial;
+
+            meshTp.Clear();
+            meshTp.vertices = chunkVerticesTp.ToArray();
+            meshTp.triangles = chunkTrianglesTp.ToArray();
+            meshTp.normals = chunkNormalsTp.ToArray();
+            meshTp.uv = chunkUVsTp.ToArray();
+            meshTp.Optimize();
+
+            chunkTpGameObj.transform.position = chunkVoxelPos;
+            chunkTpGameObj.GetComponent<Renderer>().material = TextureAtlasTransparentMaterial;
         }
     }
 
